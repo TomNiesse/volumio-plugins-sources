2c70c99a1ff552a0ba3bf0d48a3d82f521437250
Tom Niesse <25818005+TomNiesse@users.noreply.github.com>
2022-04-06 11:50:44 +0000

    Update install.sh
    added dependency "request"

--- lcd_info/install.sh
+++ lcd_info/install.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 # Install the required packages via apt-get
 sudo apt-get -y install make g++
 # Install i2c and coffee-script globally, since the volumio installer is not what it used to be
-sudo npm install i2c coffee-script
+sudo npm install i2c coffee-script request
 
 # If you need to differentiate install for armhf and i386 you can get the variable like this
 #DPKG_ARCH=`dpkg --print-architecture`
