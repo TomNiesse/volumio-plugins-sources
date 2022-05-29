106315378624a20280a7bc2500f15756f11f98c1
Tom <tomniesse@gmail.com>
2022-05-29 22:57:28 +0000

    npm fix?

--- lcd_info/install.sh
+++ lcd_info/install.sh
@@ -4,8 +4,9 @@ echo "Installing lcd info Dependencies"
 sudo apt-get update
 # Install the required packages via apt-get
 sudo apt-get -y install make g++
-# Install i2c and coffee-script globally, since the volumio installer is not what it used to be
-sudo npm install i2c coffee-script request
+# Install i2c, request and coffee-script
+npm install
+npm install i2c coffee-script request
 
 # If you need to differentiate install for armhf and i386 you can get the variable like this
 #DPKG_ARCH=`dpkg --print-architecture`
